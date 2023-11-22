92a9559f8f9b87b380332404fc0aed8c0aca1762
Blue89Azul <life.89.blue@gmail.com>
2023-11-22 11:21:28 +0900

    imported styled_text.dart

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:roll_dice_app/styled_text.dart';
 
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
@@ -12,13 +13,7 @@ class GradientContainer extends StatelessWidget {
               begin: Alignment.topLeft,
               end: Alignment.bottomRight)),
       child: const Center(
-        child: Text(
-          'Hello World',
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 28,
-          ),
-        ),
+        child: StyledText(title: 'Hello World!!'),
       ),
     );
   }
