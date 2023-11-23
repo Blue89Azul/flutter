13d751c60f24db8038e6e54d87c7d02ac4168886
Blue89Azul <life.89.blue@gmail.com>
2023-11-23 11:18:00 +0900

    change format of constructor

--- lib/styled_text.dart
+++ lib/styled_text.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class StyledText extends StatelessWidget {
   final String title;
 
-  const StyledText({super.key, required this.title});
+  const StyledText(this.title, {super.key});
 
   @override
   Widget build(BuildContext context) {
