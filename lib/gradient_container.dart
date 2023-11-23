da5a875ad925f9f19181ed62d6abe93e06a82383
Blue89Azul <life.89.blue@gmail.com>
2023-11-23 11:35:53 +0900

    added 2 parameters, which are startColor and endColor, into constructor

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -2,17 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:roll_dice_app/styled_text.dart';
 
 const Alignment startAlignment = Alignment.topLeft;
-const Alignment endAlignment = Alignment.bottomRight;
+const Alignment endAlignment   = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key});
+  final Color startColor;
+  final Color endColor;
+
+  const GradientContainer(this.startColor, this.endColor, {super.key});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: const BoxDecoration(
+      decoration: BoxDecoration(
           gradient: LinearGradient(
-        colors: [Colors.deepPurple, Colors.redAccent],
+        colors: [startColor, endColor],
         begin: startAlignment,
         end: endAlignment,
       )),
